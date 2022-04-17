696d69b792ddc5cb1d7940d358c26d3b1cf43db7
xanedesu <xanedesu@gmail.com>
2022-04-17 18:35:45 +0300

    Обновил проверку токена

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import axios from 'axios';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService: ConfigService) {
     super({
+      passReqToCallback: true,
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get('JWT_SECRET_OR_KEY'),
     });
   }
 
-  async validate(payload: any) {
-    return payload;
+  async validate(req: any, payload: any) {
+    const url = this.configService.get<string>('AUTH_URL');
+    try {
+      await axios.post(
+        url.concat(`/auth/projects/${payload.project.id}/token/verify`),
+        undefined,
+        {
+          headers: {
+            authorization: req.headers.authorization,
+          },
+        },
+      );
+
+      return payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
   }
 }
